707e23aa23cfecfafcdc70876ad8567b4cc21519
Padovani <gabrielpadov@gmail.com>
2019-02-18 18:29:58 -0300

    education component: complementary courses and other activities. Views for this component's forms

--- src/app/publisher/about-me-editor/education/education.component.ts
+++ src/app/publisher/about-me-editor/education/education.component.ts
@@ -12,10 +12,12 @@ export class EducationComponent implements OnInit {
 
   dateStart: Date;
   dateEnd: Date;
+  dateComp: Date;
   educationForm: FormGroup;
+  complementForm: FormGroup;
   submitted: boolean;
   message: string;
-  Actual = false;
+  dateDisabled = false;
 
   constructor(private fb: FormBuilder, private messageService: MessageService) { }
 
@@ -23,16 +25,35 @@ export class EducationComponent implements OnInit {
     this.educationForm = this.fb.group({
         'institution': new FormControl('', Validators.required),
         'course': new FormControl('', Validators.required),
-        'dateStart': new FormControl(''),
-        'dateEnd': new FormControl(''),
+        'dateStart': new FormControl('', Validators.required),
+        'dateEnd': new FormControl('', Validators.required),
         'description': new FormControl('', Validators.required),
         'note': new FormControl('', Validators.required)
     });
+    this.complementForm = this.fb.group({
+      'institution': new FormControl('', Validators.required),
+      'course': new FormControl('', Validators.required),
+      'dateComp': new FormControl('', Validators.required),
+      'note': new FormControl('', Validators.required)
+  });
   }
 
   get diagnostic() { return JSON.stringify(this.educationForm.value); }
+  get diagnostic1() { return JSON.stringify(this.complementForm.value); }
 
-  onSubmit(value) {
+  onEducation(value) {
+    console.log(value);
+      this.submitted = true;
+      this.messageService.add({severity: 'info', summary: 'Success',
+        detail: 'Form Submitted'});
+     // this.educationForm.reset();
+      /* this.educationService.sendMessage(value).subscribe(() => {
+        console.log('Formulario de contacto', 'Mensaje enviado correctamente', 'success');
+       });
+     } */
+  }
+
+  onComplement(value) {
     console.log(value);
       this.submitted = true;
       this.messageService.add({severity: 'info', summary: 'Success',
@@ -46,6 +67,10 @@ export class EducationComponent implements OnInit {
 
   showResponse(event) {
     this.messageService.add({severity: 'info', summary: 'Succees', detail: 'User Responded', sticky: true});
+  }
+
+  toogleDateEnd() {
+    this.dateDisabled = !this.dateDisabled;
   }
 
 }
